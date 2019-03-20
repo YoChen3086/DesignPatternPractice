f2aa27c217994ddc07b7f708fa9ad5d978ad10c4
yochen20 <d87487575107@gmail.com>
2019-03-20 22:13:07 +0800

    6.2 型男小菜第一版

---
+++ DesignPatternPractice/Decorator/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    public class Person
+    {
+        private string name;
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public void WearTShirts()
+        {
+            Console.Write("大T恤 ");
+        }
+
+        public void WearBigTrouser()
+        {
+            Console.Write("垮褲 ");
+        }
+
+        public void WearSneakers()
+        {
+            Console.Write("破球鞋 ");
+        }
+
+        public void WearSuit()
+        {
+            Console.Write("西裝 ");
+        }
+
+        public void WearTie()
+        {
+            Console.Write("領帶 ");
+        }
+
+        public void WearLeatherShoes()
+        {
+            Console.Write("皮鞋 ");
+        }
+
+        public void Show()
+        {
+            Console.Write($"裝扮的{name}");
+        }
+    }
+}
---
+++ DesignPatternPractice/Decorator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("小菜");
+
+            Console.WriteLine("第一種裝扮:");
+            person.WearTShirts();
+            person.WearBigTrouser();
+            person.WearSneakers();
+            person.Show();
+
+            Console.WriteLine("\n第二種裝扮:");
+            person.WearSuit();
+            person.WearTie();
+            person.WearLeatherShoes();
+            person.Show();
+
+            Console.ReadLine();
+        }
+    }
+}

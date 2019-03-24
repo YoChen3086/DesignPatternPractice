66440c09906023e5e39a4693b9202040146f751f
yochen20 <d87487575107@gmail.com>
2019-03-24 17:04:55 +0800

    7.2 沒有代理的程式碼

---
+++ DesignPatternPractice/Proxy/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // jiaojiao 被追求者
+            // daili 代理追求者
+            // zhuojiayi 追求者
+
+            SchoolGirl jiaojiao = new SchoolGirl();
+            jiaojiao.Name = "嬌嬌";
+
+            Pursuit zhuojiayi = new Pursuit(jiaojiao);
+
+            zhuojiayi.GiveDolls();
+            zhuojiayi.GiveFlowers();
+            zhuojiayi.GiveFlowers();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatternPractice/Proxy/SchoolGirl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Proxy
+{
+    public class SchoolGirl
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+    }
+}

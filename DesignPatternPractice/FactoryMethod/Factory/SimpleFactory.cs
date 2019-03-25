9755a10747b2d13acf8ba93dfe89de7e9da16b31
yochen20 <d87487575107@gmail.com>
2019-03-25 22:57:17 +0800

    FactoryMethod(以南丁格爾為例子)

---
+++ DesignPatternPractice/FactoryMethod/Factory/SimpleFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    [Obsolete]
+    public class SimpleFactory
+    {
+        public static Nightingale CreateNightingale(string type)
+        {
+            Nightingale result = null;
+            switch (type)
+            {
+                case "大學生":
+                    result = new Undergraduate();
+                    break;
+                case "社區義工":
+                    result = new Volunteer();
+                    break;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DesignPatternPractice/FactoryMethod/Nightingale/Nightingale.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoryMethod
+{
+    public class Nightingale
+    {
+        public void Sweep()
+        {
+            Console.WriteLine("掃地");
+        }
+
+        public void Wash()
+        {
+            Console.WriteLine("洗衣");
+        }
+
+        public void BuyRice()
+        {
+            Console.WriteLine("買米");
+        }
+    }
+}

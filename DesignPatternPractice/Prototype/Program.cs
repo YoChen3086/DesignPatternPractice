2e416f78c5ddbbea39eb56b49149153f02e92ddc
yochen20 <d87487575107@gmail.com>
2019-03-26 16:09:26 +0800

    9.2 履歷程式碼初步實現

---
+++ DesignPatternPractice/Prototype/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume resumeA = new Resume("大鳥");
+            resumeA.SetPersonlInfo("男", 29);
+            resumeA.SetWorkExperience("1998-2000", "XX公司");
+
+            Resume resumeB = new Resume("大鳥");
+            resumeB.SetPersonlInfo("男", 29);
+            resumeB.SetWorkExperience("1998-2000", "XX公司");
+
+            Resume resumeC = new Resume("大鳥");
+            resumeC.SetPersonlInfo("男", 29);
+            resumeC.SetWorkExperience("1998-2000", "XX公司");
+
+            resumeA.Display();
+            resumeB.Display();
+            resumeC.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatternPractice/Prototype/Resume.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public class Resume
+    {
+        private string name;
+        private string sex;
+        private int age;
+        private string timeArea;
+        private string company;
+
+        public Resume(string name)
+        {
+            this.name = name;
+        }
+
+        // 設定個人資訊
+        public void SetPersonlInfo(string sex, int age)
+        {
+            this.sex = sex;
+            this.age = age;
+        }
+
+        // 設定工作經歷
+        public void SetWorkExperience(string timeArea, string company)
+        {
+            this.timeArea = timeArea;
+            this.company = company;
+        }
+
+        // 顯示
+        public void Display()
+        {
+            Console.WriteLine($"{name} {sex} {age}");
+            Console.WriteLine($"工作經歷: {timeArea} {company}");
+        }
+    }
+}

a0cb151707cda355e13749da41436a2a3ae654d0
yochen20 <d87487575107@gmail.com>
2019-04-01 09:33:26 +0800

    12.2 股民炒股程式碼

---
+++ DesignPatternPractice/Facade/NationDebtOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    class NationDebtOne
+    {
+        public void Sell()
+        {
+            Console.WriteLine("公債1賣出");
+        }
+
+        public void Buy()
+        {
+            Console.WriteLine("公債1買入");
+        }
+    }
+}
---
+++ DesignPatternPractice/Facade/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StockOne stock1 = new StockOne();
+            StockTwo stock2 = new StockTwo();
+            NationDebtOne debt = new NationDebtOne();
+            RealtyOne realty = new RealtyOne();
+
+            stock1.Buy();
+            stock2.Buy();
+            debt.Buy();
+            realty.Buy();
+
+            stock1.Sell();
+            stock2.Sell();
+            debt.Sell();
+            realty.Sell();
+
+            Console.ReadLine();
+        }
+    }
+}

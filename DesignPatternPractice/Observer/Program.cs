27ce241ed20e45ee1ca4b50fb5906fad1bc4bca1
yochen20 <d87487575107@gmail.com>
2019-04-01 16:14:36 +0800

    14.2 雙向耦合的程式碼

---
+++ DesignPatternPractice/Observer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 櫃台小姐童子婕
+            Secretary secretary = new Secretary();
+
+            // 看股票的同事
+            StockObserver colleague1 = new StockObserver("同事1", secretary);
+            StockObserver colleague2 = new StockObserver("同事2", secretary);
+
+            // 櫃台記下了兩位同事
+            secretary.Attach(colleague1);
+            secretary.Attach(colleague2);
+
+            // 發現老闆回來
+            secretary.SecretartAction = "老闆回來囉!";
+
+            // 通知兩個同事
+            secretary.Notify();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DesignPatternPractice/Observer/StockObserver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    public class StockObserver
+    {
+        private string name;
+        private Secretary sub;
+
+        public StockObserver(string name, Secretary sub)
+        {
+            this.name = name;
+            this.sub = sub;
+        }
+
+        public void Update()
+        {
+            Console.WriteLine($"{sub.SecretartAction} {name} 關閉股票行情，繼續工作!");
+        }
+    }
+}

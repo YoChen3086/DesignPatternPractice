ee10eb852132e70305387cc9701cb38950174c6b
yochen20 <d87487575107@gmail.com>
2019-04-04 21:20:34 +0800

    18.2 儲存遊戲進度

---
+++ DesignPatternPractice/Memento/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 打王前
+            GameRole character = new GameRole();
+
+            character.GetInitState();
+            character.StateDisplay();
+
+            // 保存進度
+            GameRole backup = new GameRole();
+            backup.Vitality = character.Vitality;
+            backup.Attack = character.Attack;
+            backup.Defense = character.Defense;
+
+            // 打王
+            character.Fight();
+            character.StateDisplay();
+
+            // 讀取檔案
+            character.Vitality = backup.Vitality;
+            character.Attack = backup.Attack;
+            character.Defense = backup.Defense;
+
+            character.StateDisplay();
+
+            Console.ReadLine();
+        }
+    }
+}

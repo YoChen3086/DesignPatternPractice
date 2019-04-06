988b60f41803fecefb6ca979d5d334f96200ae1c
yochen20 <d87487575107@gmail.com>
2019-04-06 09:52:50 +0800

    21.4 獨體模式

---
+++ DesignPatternPractice/Singleton/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton()
+        { }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}

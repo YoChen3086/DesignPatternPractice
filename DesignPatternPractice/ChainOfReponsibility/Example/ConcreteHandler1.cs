60200f9b0f4c1cb752eced9424daf3f389931c47
yochen20 <d87487575107@gmail.com>
2019-04-07 12:40:14 +0800

    24.3 職責鏈模式

---
+++ DesignPatternPractice/ChainOfReponsibility/Example/ConcreteHandler1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfReponsibility
+{
+    public class ConcreteHandler1 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 0 && request < 10)
+            {
+                Console.WriteLine($"{this.GetType().Name} 處理請求 {request}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}
---
+++ DesignPatternPractice/ChainOfReponsibility/Example/ConcreteHandler2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChainOfReponsibility
+{
+    public class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest(int request)
+        {
+            if (request >= 10 && request < 20)
+            {
+                Console.WriteLine($"{this.GetType().Name} 處理請求 {request}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(request);
+            }
+        }
+    }
+}

9ceaf4a4eb4305ed6fe3d92742723e8cea72cc89
yochen20 <d87487575107@gmail.com>
2019-04-07 09:17:24 +0800

    23.3 緊耦合設計

---
+++ DesignPatternPractice/Command/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Command
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Barbecuer barbecuer = new Barbecuer();
+
+            barbecuer.BakeMutton();
+            barbecuer.BakeMutton();
+            barbecuer.BakeMutton();
+            barbecuer.BakeChickenWing();
+            barbecuer.BakeMutton();
+            barbecuer.BakeMutton();
+            barbecuer.BakeChickenWing();
+
+            Console.ReadLine();
+        }
+    }
+}

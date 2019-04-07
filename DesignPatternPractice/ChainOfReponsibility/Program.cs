377f5b7dd18af025422aab05880add571c718619
yochen20 <d87487575107@gmail.com>
2019-04-07 12:57:17 +0800

    24.5 加薪程式碼重構

--- DesignPatternPractice/ChainOfReponsibility/Program.cs
+++ DesignPatternPractice/ChainOfReponsibility/Program.cs
@@ -6,19 +6,37 @@ namespace ChainOfReponsibility
     {
         static void Main(string[] args)
         {
-            Handler h1 = new ConcreteHandler1();
-            Handler h2 = new ConcreteHandler2();
-            Handler h3 = new ConcreteHandler3();
+            //Handler h1 = new ConcreteHandler1();
+            //Handler h2 = new ConcreteHandler2();
+            //Handler h3 = new ConcreteHandler3();
 
-            h1.SetSuccessor(h2);
-            h2.SetSuccessor(h3);
+            //h1.SetSuccessor(h2);
+            //h2.SetSuccessor(h3);
 
-            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
+            //int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
 
-            foreach (int request in requests)
-            {
-                h1.HandleRequest(request);
-            }
+            //foreach (int request in requests)
+            //{
+            //    h1.HandleRequest(request);
+            //}
+
+            CommonManager commonManager = new CommonManager("經理");
+            Majordomo majordomo = new Majordomo("總監");
+            GeneralManager generalManager = new GeneralManager("總經理");
+            commonManager.SetSuperior(majordomo);
+            majordomo.SetSuperior(generalManager);
+
+            Request request = new Request("請假", "小菜請假", 1);
+            commonManager.RequestApplications(request);
+
+            Request request2 = new Request("請假", "小菜請假", 4);
+            commonManager.RequestApplications(request2);
+
+            Request request3 = new Request("加薪", "小菜請求加薪", 500);
+            commonManager.RequestApplications(request3);
+
+            Request request4 = new Request("加薪", "小菜請求加薪", 1000);
+            commonManager.RequestApplications(request4);
 
             Console.ReadLine();
         }

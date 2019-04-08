488d98a36e2518dfba0a81b5703210ea436ad7a7
yochen20 <d87487575107@gmail.com>
2019-04-08 13:25:49 +0800

    27.2 解譯器模式

---
+++ DesignPatternPractice/Interpreter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Context context = new Context();
+            IList<AbstractExpression> list = new List<AbstractExpression>();
+            list.Add(new TerminalExpression());
+            list.Add(new NonterminalExpression());
+            list.Add(new TerminalExpression());
+            list.Add(new TerminalExpression());
+
+            foreach (AbstractExpression exp in list)
+            {
+                exp.Interpret(context);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

1da5f5c1aa301f683467d731e38ff04a6df0e7b1
yochen20 <d87487575107@gmail.com>
2019-04-08 08:42:05 +0800

    25.2 仲介者模式

---
+++ DesignPatternPractice/Mediator/Architecture/ConcreteColleague1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mediator
+{
+    public class ConcreteColleague1 : Colleague
+    {
+        public ConcreteColleague1(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        {
+            Console.WriteLine($"同事1得到資訊: {message}");
+        }
+    }
+}
---
+++ DesignPatternPractice/Mediator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator mediator = new ConcreteMediator();
+
+            ConcreteColleague1 colleague1 = new ConcreteColleague1(mediator);
+            ConcreteColleague2 colleague2 = new ConcreteColleague2(mediator);
+
+            mediator.Colleague1 = colleague1;
+            mediator.Colleague2 = colleague2;
+
+            colleague1.Send("吃過飯了嗎?");
+            colleague2.Send("沒有呢，你打算請客?");
+
+            Console.ReadLine();
+        }
+    }
+}

13bd29578f98d78efec8cdd3cd7315688d8af2fa
yochen20 <d87487575107@gmail.com>
2019-04-09 09:16:09 +0800

    Strategy

---
+++ DesignPatternPractice/Strategy/Architecture/ConcreteStrategyA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class ConcreteStrategyA : Strategy
+    {
+        public override void AlgorithmInterface()
+        {
+            Console.WriteLine($"演算法A實現");
+        }
+    }
+}

4e028b11500c70abf96a393d9ded68007964b380
yochen20 <d87487575107@gmail.com>
2019-04-09 15:47:48 +0800

    Adapter

---
+++ DesignPatternPractice/Adapter/Architecture/Target.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter
+{
+    public class Target
+    {
+        public virtual void Request()
+        {
+            Console.WriteLine("普通請求!");
+        }
+    }
+}

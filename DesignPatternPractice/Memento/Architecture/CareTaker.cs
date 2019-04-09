bef3471d0e105186b8ba86990891168b148259fe
yochen20 <d87487575107@gmail.com>
2019-04-09 15:56:15 +0800

    Memento

---
+++ DesignPatternPractice/Memento/Architecture/CareTaker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    public class CareTaker
+    {
+        private Memento memento;
+
+        public Memento Memento
+        {
+            get { return memento; }
+            set { memento = value; }
+        }
+    }
+}

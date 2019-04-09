11f9b58da77c7f58a00d4d32fc380d42ab7ecc5b
yochen20 <d87487575107@gmail.com>
2019-04-09 09:27:52 +0800

    Decorator

---
+++ DesignPatternPractice/Decorator/Architecture/ConcreteDecoratorA.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    public class ConcreteDecoratorA : Decorator
+    {
+        // 本類別的特有功能，以有別於ConcreateDecoratorB
+        private string addedState;
+
+        public override void Operation()
+        {
+            base.Operation();
+            addedState = "New State";
+            Console.WriteLine("具體裝飾物件A的操作");
+        }
+    }
+}
---
+++ DesignPatternPractice/Decorator/Architecture/Decorator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+   public abstract class Decorator : Component
+    {
+        protected Component component;
+
+        public void SetComponent(Component component)
+        {
+            this.component = component;
+        }
+
+        // 重寫Operation()，實際執行的是Component的Operation()
+        public override void Operation()
+        {
+            if (component != null)
+            {
+                component.Operation();
+            }
+        }
+    }
+}

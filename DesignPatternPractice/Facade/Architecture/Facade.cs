035e2f9f8e5ec8c9b57ca915908dbf120bba5094
yochen20 <d87487575107@gmail.com>
2019-04-09 12:13:29 +0800

    Facade

---
+++ DesignPatternPractice/Facade/Architecture/Facade.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class Facade
+    {
+        // 外觀類別，他需要了解所有的子系統的方法或屬性，進行組合，以備外界調用
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine($"\n方法組A() ---");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+
+        public void MethodB()
+        {
+            Console.WriteLine($"\n方法組B() ---");
+            three.MethodThree();
+            four.MethodFour();
+        }
+    }
+}
---
+++ DesignPatternPractice/Facade/Architecture/SubSystemThree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facade
+{
+    public class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine(" 子系統方法三");
+        }
+    }
+}
